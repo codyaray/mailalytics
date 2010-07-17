d549da633c486f80bf21492ccb8b574fb2729101
Cody A. Ray <sites@codyaray.com>
2010-07-17 17:27:57 -0400

    updated with msg_count msg_is_reply

--- mailalytics.rb
+++ mailalytics.rb
@@ -3,18 +3,38 @@
 require 'rubygems'
 require 'pathname'
 require 'rmail'
+require 'pp'
+require 'ostruct'
 
 count = 0
-filename = "2010-May.txt.gz"
-File.open(Pathname.new(filename), 'r') do |mbox|
-  RMail::Mailbox.parse_mbox(mbox) do |raw|
-    count += 1
-    print "#{count} mails\n"
-    begin
+stats = {}
+mbox_files = Dir.glob("Mbox_Files/*.txt")
+mbox_files.each do |filename|
+  File.open(Pathname.new(filename), 'r') do |mbox|
+    RMail::Mailbox.parse_mbox(mbox) do |raw|
+      count += 1
+
       message = RMail::Parser.read(raw)
-      puts message.header
-    rescue NoMethodError
-      print "Couldn't parse date header, ignoring broken spam mail\n"
+
+      name = message.header["From"]
+      pp message and continue if name == nil
+      name[/\((.*)\)/]
+      name = $1
+
+      if stats.has_key? name
+        record = stats[name]
+        record.msg_count += 1
+        record.msg_is_reply += 1
+      else
+        record = OpenStruct.new
+        record.msg_count = 1
+        record.msg_is_reply = message.header.field?("In-Reply-To") ? 1 : 0
+        stats[name] = record
+      end
     end
   end
 end
+
+pp stats
+
+print "#{count} mails\n"
