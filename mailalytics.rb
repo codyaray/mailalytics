46446af2c8b4dfaad3d2bc054c055039a7be446a
Cody A. Ray <sites@codyaray.com>
2010-07-18 20:09:57 -0400

    updated to avoid the weird CSV formatting errors with \n and quotes. stupid mistake. Ruby's p operator calls .inspect rather than .to_str under the hood

--- mailalytics.rb
+++ mailalytics.rb
@@ -1,40 +1,43 @@
 #!/usr/bin/ruby -w
 
 require 'rubygems'
-require 'pathname'
-require 'rmail'
-require 'pp'
 require 'ostruct'
+require 'rmail'
+require 'csv'
 
-count = 0
 stats = {}
 mbox_files = Dir.glob("Mbox_Files/*.txt")
 mbox_files.each do |filename|
-  File.open(Pathname.new(filename), 'r') do |mbox|
+  File.open(filename, 'r') do |mbox|
     RMail::Mailbox.parse_mbox(mbox) do |raw|
-      count += 1
-
       message = RMail::Parser.read(raw)
 
       name = message.header["From"]
       pp message and continue if name == nil
       name[/\((.*)\)/]
       name = $1
+      
+      reply_count_diff = message.header.field?("In-Reply-To") ? 1 : 0
 
       if stats.has_key? name
         record = stats[name]
         record.msg_count += 1
-        record.msg_is_reply += 1
+        record.msg_reply_count += reply_count_diff
       else
         record = OpenStruct.new
         record.msg_count = 1
-        record.msg_is_reply = message.header.field?("In-Reply-To") ? 1 : 0
+        record.msg_reply_count = reply_count_diff
         stats[name] = record
       end
     end
   end
 end
 
-pp stats
+src = []
+src << ["Name", "Total Msg Count", "Msg Reply Count"]
+stats.each { |r| src << [r[0], r[1].msg_count.to_s, r[1].msg_reply_count.to_s] }
 
-print "#{count} mails\n"
+buf = ''
+src.each { |row| CSV.generate_row(row, 3, buf) }
+
+puts buf
