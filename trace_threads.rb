902f1503da1109169682f34862fca1a3e39bdb59
bluepojo <bluepojo@gmail.com>
2010-07-31 18:11:00 -0400

    Read from stdin

--- trace_threads.rb
+++ trace_threads.rb
@@ -7,32 +7,28 @@ require 'pp'
 require 'tree'
 
 module Mailalytics
-  def trace_threads
+  def trace_threads mail_data
     messages = Hash.new
     roots = []
-    mbox_files = Dir.glob("Mbox_Files/*.txt")
-    mbox_files.each do |filename|
-      File.open(filename, 'r') do |mbox|
-        RMail::Mailbox.parse_mbox(mbox) do |raw|
-          message = RMail::Parser.read(raw)
-          
-          message_id = message.header["Message-ID"]
-          messages[message_id] = Tree::TreeNode.new(message_id, message)
-
+    RMail::Mailbox.parse_mbox(mail_data) do |raw|
+      message = RMail::Parser.read(raw)
+      
+      message_id = message.header["Message-ID"]
+      messages[message_id] = Tree::TreeNode.new(message_id, message)
+      
           reply_to = message.header["In-Reply-To"]
-          
-          if not reply_to
-            roots << messages[message_id]
-          else
-            begin
-              messages[reply_to] << messages[message_id] if messages.has_key? reply_to
-            rescue
-              next
-            end
-          end
+      
+      if not reply_to
+        roots << messages[message_id]
+      else
+        begin
+          messages[reply_to] << messages[message_id] if messages.has_key? reply_to
+        rescue
+          next
         end
       end
     end
+    
     sizes = Array.new(100, 0)
     roots.each do |m|
       size = m.size
@@ -41,13 +37,14 @@ module Mailalytics
     sizes
   end
 
-  def thread_length_csv
-    rows = trace_threads
+  def thread_length_csv mail_data
+    rows = trace_threads mail_data
     puts 'length of thread, number of threads with said length'
-    rows.each_index { |i| puts "#{i.to_s}, #{rows[i].to_s}" if rows[i] != 0 }
+    rows.each_index { |i| puts "#{i.to_s}, #{rows[i].to_s}" }
   end
 
 end
 
 include Mailalytics
-thread_length_csv
+mail_data = $stdin.readlines.join("\r\n")
+thread_length_csv mail_data
